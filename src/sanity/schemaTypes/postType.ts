c38fc8f2e9f6832d8c1866d2fb9af0002a94a7d8
sQoooop <matthias.obrist@hotmail.com>
2025-11-13 21:20:29 +0100

    added images

--- src/sanity/schemaTypes/postType.ts
+++ src/sanity/schemaTypes/postType.ts
@@ -50,6 +50,16 @@ export const postType = defineType({
       name: 'body',
       type: 'blockContent',
     }),
+    defineField({
+  name: 'alt',
+  type: 'string',
+  title: 'Alternative text',
+  validation: rule => rule.custom((value, context) => {
+    const parent = context?.parent as {asset?: {_ref?: string}}
+
+    return !value && parent?.asset?._ref ? 'Alt text is required when an image is present' : true
+  }),
+})
   ],
   preview: {
     select: {
